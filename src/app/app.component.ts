e077fe1a86bae8ccbedafe4bb0c2cc1bf11ae0fc
Dogue Dogue <rvilches38@yahoo.com.mx>
2022-01-29 14:46:55 -0600

    ngFor recorrer objetos, interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Product} from './product.model'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +11,51 @@ export class AppComponent {
   title = 'dogue-store';
 
   items = ['shirt', 'coat', 'pants', 'shoes'];
+
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 4000,
+      description: 'bla bla bla bla bla' 
+    },
+    {
+      id: '2',
+      image: 'assets/images/hoodie.png',
+      title: 'Hoodie',
+      price: 4500,
+      description: 'bla bla bla bla bla' 
+    },
+    {
+      id: '3',
+      image: 'assets/images/mug.png',
+      title: 'Mug',
+      price: 3700,
+      description: 'bla bla bla bla bla' 
+    },
+    {
+      id: '4',
+      image: 'assets/images/pin.png',
+      title: 'Pin',
+      price: 100,
+      description: 'bla bla bla bla bla' 
+    },
+    {
+      id: '5',
+      image: 'assets/images/stickers1.png',
+      title: 'Stickers',
+      price: 250,
+      description: 'bla bla bla bla bla' 
+    },
+    {
+      id: '6',
+      image: 'assets/images/stickers2.png',
+      title: 'Stickers',
+      price: 250,
+      description: 'bla bla bla bla bla' 
+    }
+  ];
 
   addItem(){
     this.items.push(this.title);
