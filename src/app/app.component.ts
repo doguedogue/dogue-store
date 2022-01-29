b743b27e6dec8d6d7ffc37abb2f106c56a8fdeb2
Dogue Dogue <rvilches38@yahoo.com.mx>
2022-01-29 14:06:11 -0600

    ngFor, push and splice in array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dogue-store';
+
+  items = ['shirt', 'coat', 'pants', 'shoes'];
+
+  addItem(){
+    this.items.push(this.title);
+  }
+
+  deleteItem(index: number){
+    this.items.splice(index, 1);
+  }
 }
