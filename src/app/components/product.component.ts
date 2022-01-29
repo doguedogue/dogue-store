285ab79d707eca44bfa0289feaf43b33e0638610
Dogue Dogue <rvilches38@yahoo.com.mx>
2022-01-29 15:20:18 -0600

    Componentes y Decoradores

---
+++ src/app/components/product.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core'
+
+import { Product} from '../product.model'
+
+@Component({
+    selector: 'app-product',
+    templateUrl: './product.component.html'
+
+})
+
+export class ProductComponent {
+
+    product: Product = {
+        id: '1',
+        image: 'assets/images/camiseta.png',
+        title: 'Camiseta',
+        price: 4000,
+        description: 'bla bla bla bla bla' 
+    };
+}
