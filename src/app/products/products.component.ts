31db9b9dfdd2242a76b40417bdd5c44fb7f9d30f
Dogue Dogue <rvilches38@yahoo.com.mx>
2022-01-30 19:25:14 -0600

    Servicio y detalle del producto 1/2

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
 
-import { Product} from '../product.model'
+import {ProductsService} from '../products.service'
 
 @Component({
   selector: 'app-products',
@@ -8,51 +9,14 @@ import { Product} from '../product.model'
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-  products: Product[] = [
-    {
-      id: '1',
-      image: 'assets/images/camiseta.png',
-      title: 'Camiseta',
-      price: 4000,
-      description: 'bla bla bla bla bla' 
-    },
-    {
-      id: '2',
-      image: 'assets/images/hoodie.png',
-      title: 'Hoodie',
-      price: 4500,
-      description: 'bla bla bla bla bla' 
-    },
-    {
-      id: '3',
-      image: 'assets/images/mug.png',
-      title: 'Mug',
-      price: 3700,
-      description: 'bla bla bla bla bla' 
-    },
-    {
-      id: '4',
-      image: 'assets/images/pin.png',
-      title: 'Pin',
-      price: 100,
-      description: 'bla bla bla bla bla' 
-    },
-    {
-      id: '5',
-      image: 'assets/images/stickers1.png',
-      title: 'Stickers',
-      price: 250,
-      description: 'bla bla bla bla bla' 
-    },
-    {
-      id: '6',
-      image: 'assets/images/stickers2.png',
-      title: 'Stickers',
-      price: 250,
-      description: 'bla bla bla bla bla' 
-    }
-  ];
-  constructor() { }
+  
+  products: Product[];
+
+  constructor(
+    private productsService: ProductsService
+  ) { 
+    this.products = productsService.getAllProducts();
+  }
 
   ngOnInit(): void {
   }
