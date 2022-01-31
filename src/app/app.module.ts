669c5d081f5e411652733af0dafd9a535df16cbb
Dogue Dogue <rvilches38@yahoo.com.mx>
2022-01-31 01:31:30 -0600

    shared module y core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,31 +9,25 @@ import localeEs from '@angular/common/locales/es-MX';
 import { registerLocaleData } from '@angular/common';
 registerLocaleData(localeEs);
 
-import { ProductComponent } from './components/product.component';
+import { ProductComponent } from './product/product.component';
 import { CartComponent } from './cart/cart.component';
-import { ExponentialPipe } from './exponential.pipe';
-import { HighlightDirective } from './highlight.directive';
 import { ProductsComponent } from './products/products.component';
 import { ContactComponent } from './contact/contact.component';
 import { DemoComponent } from './demo/demo.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { LayoutComponent } from './layout/layout.component'
+import { SharedModule } from './shared/shared.module'
+import { CoreModule } from './core/core.module'
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     CartComponent,
-    ExponentialPipe,
-    HighlightDirective,
     ProductsComponent,
     ContactComponent,
     DemoComponent,
-    HeaderComponent,
-    FooterComponent,
     PageNotFoundComponent,
     ProductDetailComponent,
     LayoutComponent
@@ -41,7 +35,9 @@ import { LayoutComponent } from './layout/layout.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    SharedModule,
+    CoreModule
   ],
   providers:  [ { provide: LOCALE_ID, useValue: 'es-MX' } ],
   bootstrap: [AppComponent]
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,8 @@ import {BannerComponent } from './components/banner/banner.component'
 import { HomeComponent } from './components/home/home.component'
 import { HomeRoutingModule } from './home-routing.module'
 
+import { SharedModule } from './../shared/shared.module'
+
 @NgModule({
     declarations: [
         BannerComponent,
@@ -12,7 +14,8 @@ import { HomeRoutingModule } from './home-routing.module'
     ],
     imports: [
         HomeRoutingModule,
-        CommonModule
+        CommonModule,
+        SharedModule
     ]
 })
 
